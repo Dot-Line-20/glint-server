c62d9a60ca968d22ad1c5c00f19e167b90e6a0c5
kraccoon-dev <kraccoon@kakao.com>
2022-07-10 13:07:13 +0900

    refactor: router dynamic import

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,8 @@
-import users from './users'
-import root from './root'
-import auth from './auth'
+import { readdirSync } from 'fs'
+import type { IRouters } from 'types/index'
 
-export default [root, users, auth]
+export default (): IRouters[] =>
+  readdirSync(__dirname, { withFileTypes: true })
+    .filter((route) => !route.isFile())
+    .map((route) => require(`./${route.name}`))
+    .map((route) => route.default)
