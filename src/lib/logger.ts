289770aebaaa7ac660d55128b82dc2f540be69cb
kraccoon-dev <kraccoon@kakao.com>
2022-07-13 13:18:32 +0900

    console logger add colorize

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,8 +1,9 @@
 import { join } from 'path'
 import * as winston from 'winston'
 import DailyRotateFile from 'winston-daily-rotate-file'
+import { NODE_ENV } from 'lib/config'
 
-const { combine, timestamp, printf } = winston.format
+const { combine, timestamp, printf, colorize } = winston.format
 
 const fileFormat = printf(
   ({ level, message, timestamp }) => `${timestamp} [${level}] ${message}`
@@ -19,11 +20,14 @@ const dailyRoateFileTransport: DailyRotateFile = new DailyRotateFile({
   format: combine(timestamp({ format: 'HH:mm:ss' }), fileFormat),
 })
 
+const consoleTransport = new winston.transports.Console({
+  level: NODE_ENV === 'production' ? 'info' : 'debug',
+  format: combine(
+    printf(({ level, message }) => `${level} ${message}`),
+    colorize()
+  ),
+})
+
 export default winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-    dailyRoateFileTransport,
-  ],
+  transports: [consoleTransport, dailyRoateFileTransport],
 })
